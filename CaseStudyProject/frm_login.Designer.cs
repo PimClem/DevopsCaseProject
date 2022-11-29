499153bae59e7bb52cf879a398c42da90c3a17d9
PimClem <r0882182@student.thomasmore.be>
2022-11-29 12:56:18 +0100

    testpush

---
+++ CaseStudyProject/frm_login.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace CaseStudyProject
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_login = new System.Windows.Forms.Button();
+            this.lbl_user = new System.Windows.Forms.Label();
+            this.lbl_pass = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(74, 234);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(94, 29);
+            this.btn_login.TabIndex = 0;
+            this.btn_login.Text = "Log in";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // lbl_user
+            // 
+            this.lbl_user.AutoSize = true;
+            this.lbl_user.Location = new System.Drawing.Point(74, 101);
+            this.lbl_user.Name = "lbl_user";
+            this.lbl_user.Size = new System.Drawing.Size(75, 20);
+            this.lbl_user.TabIndex = 1;
+            this.lbl_user.Text = "Username";
+            // 
+            // lbl_pass
+            // 
+            this.lbl_pass.AutoSize = true;
+            this.lbl_pass.Location = new System.Drawing.Point(74, 154);
+            this.lbl_pass.Name = "lbl_pass";
+            this.lbl_pass.Size = new System.Drawing.Size(70, 20);
+            this.lbl_pass.TabIndex = 2;
+            this.lbl_pass.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 4;
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_pass);
+            this.Controls.Add(this.lbl_user);
+            this.Controls.Add(this.btn_login);
+            this.Name = "frm_login";
+            this.Text = "Bibliotheek manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label lbl_user;
+        private System.Windows.Forms.Label lbl_pass;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ CaseStudyProject/frm_management.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace CaseStudyProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_toevoegen1 = new System.Windows.Forms.Button();
+            this.btn_verwijderen1 = new System.Windows.Forms.Button();
+            this.btn_logOut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_toevoegen1
+            // 
+            this.btn_toevoegen1.Location = new System.Drawing.Point(660, 77);
+            this.btn_toevoegen1.Name = "btn_toevoegen1";
+            this.btn_toevoegen1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btn_toevoegen1.Size = new System.Drawing.Size(103, 29);
+            this.btn_toevoegen1.TabIndex = 0;
+            this.btn_toevoegen1.Text = "Toevoegen";
+            this.btn_toevoegen1.UseVisualStyleBackColor = true;
+            this.btn_toevoegen1.Click += new System.EventHandler(this.btn_toevoegen1_Click);
+            // 
+            // btn_verwijderen1
+            // 
+            this.btn_verwijderen1.Location = new System.Drawing.Point(660, 112);
+            this.btn_verwijderen1.Name = "btn_verwijderen1";
+            this.btn_verwijderen1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btn_verwijderen1.Size = new System.Drawing.Size(103, 29);
+            this.btn_verwijderen1.TabIndex = 1;
+            this.btn_verwijderen1.Text = "Verwijderen";
+            this.btn_verwijderen1.UseVisualStyleBackColor = true;
+            this.btn_verwijderen1.Click += new System.EventHandler(this.btn_verwijderen1_Click);
+            // 
+            // btn_logOut
+            // 
+            this.btn_logOut.Location = new System.Drawing.Point(685, 12);
+            this.btn_logOut.Name = "btn_logOut";
+            this.btn_logOut.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btn_logOut.Size = new System.Drawing.Size(103, 29);
+            this.btn_logOut.TabIndex = 2;
+            this.btn_logOut.Text = "Uitloggen";
+            this.btn_logOut.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_logOut);
+            this.Controls.Add(this.btn_verwijderen1);
+            this.Controls.Add(this.btn_toevoegen1);
+            this.Name = "Form1";
+            this.Text = "Bibliotheek Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_toevoegen1;
+        private System.Windows.Forms.Button btn_verwijderen1;
+        private System.Windows.Forms.Button btn_logOut;
+    }
+}
+
