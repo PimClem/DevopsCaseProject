e2864f190ddbac8da594320c7f91784ba9c0988f
PimClem <r0882182@student.thomasmore.be>
2022-11-29 15:44:25 +0100

    2911_1544

--- CaseStudyProject/frm_management.Designer.cs
+++ CaseStudyProject/frm_management.Designer.cs
@@ -29,34 +29,21 @@ namespace CaseStudyProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_toevoegen1 = new System.Windows.Forms.Button();
-            this.btn_verwijderen1 = new System.Windows.Forms.Button();
+            this.btn_aanpassen = new System.Windows.Forms.Button();
             this.btn_logOut = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btn_toevoegen1
+            // btn_aanpassen
             // 
-            this.btn_toevoegen1.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_toevoegen1.Location = new System.Drawing.Point(660, 77);
-            this.btn_toevoegen1.Name = "btn_toevoegen1";
-            this.btn_toevoegen1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btn_toevoegen1.Size = new System.Drawing.Size(128, 29);
-            this.btn_toevoegen1.TabIndex = 0;
-            this.btn_toevoegen1.Text = "Toevoegen";
-            this.btn_toevoegen1.UseVisualStyleBackColor = true;
-            this.btn_toevoegen1.Click += new System.EventHandler(this.btn_toevoegen1_Click);
-            // 
-            // btn_verwijderen1
-            // 
-            this.btn_verwijderen1.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_verwijderen1.Location = new System.Drawing.Point(660, 112);
-            this.btn_verwijderen1.Name = "btn_verwijderen1";
-            this.btn_verwijderen1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btn_verwijderen1.Size = new System.Drawing.Size(128, 29);
-            this.btn_verwijderen1.TabIndex = 1;
-            this.btn_verwijderen1.Text = "Verwijderen";
-            this.btn_verwijderen1.UseVisualStyleBackColor = true;
-            this.btn_verwijderen1.Click += new System.EventHandler(this.btn_verwijderen1_Click);
+            this.btn_aanpassen.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_aanpassen.Location = new System.Drawing.Point(660, 77);
+            this.btn_aanpassen.Name = "btn_aanpassen";
+            this.btn_aanpassen.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btn_aanpassen.Size = new System.Drawing.Size(128, 29);
+            this.btn_aanpassen.TabIndex = 0;
+            this.btn_aanpassen.Text = "Aanpassen";
+            this.btn_aanpassen.UseVisualStyleBackColor = true;
+            this.btn_aanpassen.Click += new System.EventHandler(this.btn_toevoegen1_Click);
             // 
             // btn_logOut
             // 
@@ -76,8 +63,7 @@ namespace CaseStudyProject
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btn_logOut);
-            this.Controls.Add(this.btn_verwijderen1);
-            this.Controls.Add(this.btn_toevoegen1);
+            this.Controls.Add(this.btn_aanpassen);
             this.Name = "Form1";
             this.Text = "Bibliotheek Manager";
             this.ResumeLayout(false);
@@ -86,8 +72,7 @@ namespace CaseStudyProject
 
         #endregion
 
-        private System.Windows.Forms.Button btn_toevoegen1;
-        private System.Windows.Forms.Button btn_verwijderen1;
+        private System.Windows.Forms.Button btn_aanpassen;
         private System.Windows.Forms.Button btn_logOut;
     }
 }
